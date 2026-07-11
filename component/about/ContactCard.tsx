1df8179bd1c5211c28ac172723f33240e251ff7f
Ashish Kumar Soni <106304040+DASHISH498@users.noreply.github.com>
2026-07-11 18:49:18 +0530

    button fix about page

--- component/about/ContactCard.tsx
+++ component/about/ContactCard.tsx
@@ -10,19 +10,20 @@ export default function ContactCard() {
           Contact
         </p>
 
-        <h2 className="mt-5 lg:mt-6 text-3xl sm:text-4xl font-bold leading-tight">
-          Let's Work
-          <br />
-          Together
-        </h2>
-      </div>
-
-      <button className="w-12 h-12 lg:w-14 lg:h-14 rounded-full bg-[#8B5CF6] flex items-center justify-center hover:bg-[#7C3AED] transition-all duration-300">
+        <div className="flex flex-center justify-between">
+          <h2 className="mt-5 lg:mt-6 text-3xl sm:text-4xl font-bold leading-tight">
+            Let's Work
+            <br />
+            Together
+          </h2>
+          <button className="w-12 h-12 lg:w-14 lg:h-14 rounded-full bg-[#8B5CF6] flex items-center justify-center hover:bg-[#7C3AED] transition-all duration-300">
         <ArrowUpRight
           size={20}
           className="lg:w-[22px] lg:h-[22px]"
         />
       </button>
+        </div>
+      </div>
 
     </Card>
   );
